37b46ede6f8211700d8c2a4030e3a8a4dbf76b3d
RaphGL <raphfl.dev@gmail.com>
2022-04-24 22:28:19 +0100

    added checks to symlinking status

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,44 @@
+use clap::{arg, Command};
 mod symlink;
 
 fn main() {
-    symlink::remove("testest");
+    let matches = Command::new("Tuckr")
+        .version("0.1")
+        .author("RaphGL")
+        .about("Super powered GNU Stow replacement")
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("add")
+                .about("Add symlinks to the dotfile folder")
+                .arg(arg!(<PROGRAM>))
+                .arg(arg!([FILE]...)),
+        )
+        .subcommand(
+            Command::new("remove")
+                .about("Remove symlinks from the dotfile folder")
+                .arg(arg!(<PROGRAM>))
+                .arg(arg!([FILE]...)),
+        )
+        .subcommand(Command::new("status").about("Check symlink status"))
+        .subcommand(Command::new("set").about("Run hooks").arg(arg!([HOOK])))
+        .subcommand(Command::new("init").about("Initialize a dotfile folder"))
+        .subcommand(Command::new("from-stow").about("Converts a stow repo into a tuckr one"))
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("status", _)) => symlink::get_status(),
+        Some(("add", submatches)) => {
+            symlink::add(
+                submatches.values_of("PROGRAM").unwrap(),
+                submatches.values_of("FILE").unwrap(),
+            );
+        }
+        Some(("remove", submatches)) => {
+            symlink::remove(
+                submatches.values_of("PROGRAM").unwrap(),
+                submatches.values_of("FILE").unwrap(),
+            );
+        }
+        _ => unreachable!(),
+    }
 }
--- src/symlink.rs
+++ src/symlink.rs
@@ -1,66 +1,126 @@
-use colored::*;
+use colored::Colorize;
+use core::panic;
 use dirs;
+use std::fmt::Result;
+use std::fs;
 use std::os::unix::fs::symlink;
-use std::{env, fs, path};
+use std::path::{Path, PathBuf};
 
-//  Returns tuple with symlinked files(0) and unsymlinnked files (1)
-pub fn get(path: &str) -> (Vec<path::PathBuf>, Vec<path::PathBuf>) {
-    let files = fs::read_dir(path).unwrap();
-    let mut dots_sym = Vec::new();
-    let mut dots_nsym = Vec::new();
-    for i in files {
-        let path = i.ok().unwrap().path();
-        match fs::read_link(&path) {
-            Ok(_) => dots_sym.push(path),
-            Err(_) => dots_nsym.push(path),
+struct SymlinkStatus {
+    symlinked: Vec<PathBuf>,
+    notsymlink: Vec<PathBuf>,
+}
+
+impl SymlinkStatus {
+    // Initializes struct
+    fn new() -> SymlinkStatus {
+        SymlinkStatus {
+            symlinked: Vec::new(),
+            notsymlink: Vec::new(),
         }
     }
 
-    (dots_sym, dots_nsym)
-}
+    // Populates SymlinkStatus with info
+    fn retrieve_info(&mut self) {
+        // find the dotfile's path
+        let dotfiles = (|| {
+            let home = dirs::home_dir().unwrap();
+            for f in fs::read_dir(home).unwrap() {
+                let file = f.unwrap();
+                let filepath = file.path();
+                let filename = filepath.to_str().unwrap();
+                if filename.contains("Dotfiles")
+                    || filename.contains("dotfiles")
+                    || filename.contains(".dotfiles")
+                {
+                    return Some(filepath);
+                }
+            }
+            None
+        })()
+        .unwrap();
 
-// Prints the status of the symlinks
-pub fn print() {
-    let dots = get(".");
-    println!("Symlinked files:");
-    for f in dots.0 {
-        println!("\t{}", f.to_str().unwrap().green());
+        // read the config of each program
+        for program in
+            fs::read_dir(format!("{}/{}", dotfiles.to_str().unwrap(), "configs")).unwrap()
+        {
+            let p = program.unwrap();
+            if let Ok(config) = fs::read_dir(p.path()) {
+                for conf in config {
+                    let c = conf.unwrap();
+                    if is_valid_symlink(c.path().clone()) {
+                        self.symlinked.push(c.path());
+                    } else {
+                        self.notsymlink.push(c.path());
+                    }
+                }
+            }
+        }
     }
-    println!("Not symlinked files:");
-    for f in dots.1 {
-        println!("\t{}", f.to_str().unwrap().red());
+
+    // Retrieve symlinked filenames
+    fn print_symlinked(&self) {
+        if self.symlinked.len() > 0 {
+            println!("Symlinked files:");
+            for f in &self.symlinked {
+                println!("\t{}", f.to_str().unwrap().green());
+            }
+        }
     }
-}
-
-// checks if a file is a valid dotfile entry before doing actions
-fn check_validity() -> bool {true}
-
-// finds all files in a dir and then symlinks them to home
-pub fn create(fpath: &str) {
-    let cur_path = env::current_dir().ok().unwrap();
-    let cur_dir = fs::read_dir(fpath).ok();
-    let home = dirs::home_dir().unwrap();
-    if let Some(dir) = cur_dir {
-        for d in dir {
-            let dir = d.unwrap();
-            let og_file = format!("{}/{}", cur_path.display(), dir.path().display());
-            let file_path = dir.path();
-            let filename = file_path.to_str().unwrap().split("/").last().unwrap();
-            let dest_file = format!("{}/{}", home.display(), filename);
-            let _ = symlink(og_file, dest_file);
+    // Retrieve non symlinked filenames
+    fn print_notsymlinked(self) {
+        if self.notsymlink.len() > 0 {
+            println!("Not symlinked files:");
+            for f in &self.notsymlink {
+                println!("\t{}", f.to_str().unwrap().red());
+            }
         }
     }
 }
 
-pub fn remove(fpath: &str) {
-    if let Some(home_dir)= dirs::home_dir() {
-        // TODO make to only read dotfile dir | checks default locations 
-        let dir = fs::read_dir(fpath).ok().unwrap();
-        for d in dir {
-            let file_path = d.ok().unwrap().path();
-            let filename = file_path.to_str().unwrap().split("/").last().unwrap();
-            let target_file = format!("{}/{}", home_dir.display(), filename);
-            let _ = fs::remove_file(target_file);
-        }
+// Returns true if Dotfiles file matches file in $HOME
+fn is_valid_symlink(file: PathBuf) -> bool {
+    let fpath = file.to_str().unwrap();
+
+    // strips away $HOME/Dotfiles/program from string
+    let new_path: (&str, &str);
+    if fpath.contains("Dotfiles") {
+        new_path = fpath.split_once("Dotfiles/").unwrap();
+    } else if fpath.contains("dotfiles") {
+        new_path = fpath.split_once("dotfiles/").unwrap();
+    } else if fpath.contains(".dotfiles") {
+        new_path = fpath.split_once(".dotfiles/").unwrap();
+    } else {
+        return false;
     }
+    let mut new_path = new_path.1.split_once("/").unwrap();
+    new_path = new_path.1.split_once("/").unwrap();
+
+    // appends home to new_path
+    let path_with_home = dirs::home_dir().unwrap();
+    let path_with_home = path_with_home.to_str().unwrap().to_owned() + "/" + new_path.1;
+    let f = Path::new(&path_with_home);
+    // returns true if file is a symlink and is on Dotfiles
+    if f.exists() && f.read_link().is_ok() {
+        true
+    } else {
+        false
+    }
+}
+
+pub fn get_status() {
+    let mut symstruct = SymlinkStatus::new();
+    symstruct.retrieve_info();
+    symstruct.print_symlinked();
+    symstruct.print_notsymlinked();
+}
+
+// Symlink files
+pub fn add(program_name: clap::Values, files: clap::Values) {
+    // TODO
+}
+
+// Remove symlink from files
+pub fn remove(program_name: clap::Values, files: clap::Values) {
+    // TODO
 }
