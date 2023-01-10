144988535d32139acd1d139d0010d555122cee57
RaphGL <raphfl.dev@gmail.com>
2023-01-10 17:39:44 -0100

    fixed #4: now secrets are fully functional

--- src/secrets.rs
+++ src/secrets.rs
@@ -1,12 +1,10 @@
 use crate::utils;
-use chacha20poly1305::{
-     aead::Aead, AeadCore, KeyInit, XChaCha20Poly1305,
-};
+use chacha20poly1305::{aead::Aead, AeadCore, KeyInit, XChaCha20Poly1305};
 use rand::rngs::OsRng;
 use sha2::{Digest, Sha256};
 use std::collections::HashSet;
 use std::fs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use zeroize::Zeroize;
 
 struct SecretsHandler {
@@ -47,7 +45,13 @@ impl SecretsHandler {
 
     fn encrypt(&self, dotfile: &str) -> Vec<u8> {
         let cipher = XChaCha20Poly1305::new(&self.key);
-        let dotfile = fs::read(dotfile).unwrap();
+        let dotfile = match fs::read(dotfile) {
+            Ok(f) => f,
+            Err(_) => {
+                eprintln!("No such file or directory: {dotfile}");
+                std::process::exit(1);
+            }
+        };
 
         cipher.encrypt(&self.nonce, dotfile.as_slice()).unwrap()
     }
@@ -72,12 +76,24 @@ pub fn encrypt_cmd(group: &str, dotfiles: &[String] /*, exclude: &[String]*/) {
         fs::create_dir_all(&dest_dir).unwrap();
     }
 
+    let home_dir = dirs::home_dir().unwrap();
+
     for dotfile in dotfiles {
         let mut encrypted = handler.encrypt(dotfile);
         let mut encrypted_file = handler.nonce.to_vec();
+
+        let target_file = Path::new(dotfile).canonicalize().unwrap();
+        let target_file = target_file.strip_prefix(&home_dir).unwrap();
+
+        let mut dir_path = target_file.clone().to_path_buf();
+        dir_path.pop();
+
+        // makes sure all parent directories of the dotfile are created
+        fs::create_dir_all(dest_dir.join(dir_path)).unwrap();
+
         // appends a 24 byte nonce to the beginning of the file
         encrypted_file.append(&mut encrypted);
-        fs::write(dest_dir.join(dotfile), encrypted_file).unwrap();
+        fs::write(dest_dir.join(target_file), encrypted_file).unwrap();
     }
 }
 
@@ -92,7 +108,7 @@ pub fn decrypt_cmd(group: &str) {
         let decrypted = handler.decrypt(secret.path().to_str().unwrap());
 
         fs::write(
-            dest_dir.join(secret.file_name().to_str().unwrap().to_string() + "2"),
+            dest_dir.join(secret.file_name().to_str().unwrap().to_string()),
             decrypted,
         )
         .unwrap();
